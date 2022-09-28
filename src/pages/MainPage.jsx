c82d59187259bb80c766fe0ab6eb7302c0eafdfb
jeongseoe <gldi1633@naver.com>
2022-09-28 17:18:11 +0900

    feat : 헤더 기능 수정중으로 보이는 화면어떤지 확인중임

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 const MainPage = () => {
     return (
         <div>
+            <Header />
             <div>메인 페이지입니다~</div>
             <Footer />
         </div>
