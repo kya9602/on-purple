27c82cc78c8898a11218b2f9d39430398d957660
jeongseoe <gldi1633@naver.com>
2022-10-01 21:29:27 +0900

    feat : 로그아웃 기능 구현 완료

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,14 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import profile from "../modules/profile";
 import detailSlice from "../modules/mypage";
-import post from "../modules/board"
+import post from "../modules/board";
+import logout from "../modules/user";
 
 const store = configureStore({
     reducer: {
         profile,
         post,
         mypage: detailSlice.actions,
-
+        logout
     }
 })
 
