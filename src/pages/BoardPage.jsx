695e40862ef7b4ead674a9db30d55ab7e2f31045
Mintaek2 <789rkd@naver.com>
2022-10-03 22:11:02 +0900

    fix : board에 header 추가

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import List from "../components/Board/List";
-
+import Header from "../components/Header/Header"
 const Board = () => {
 
     return (
         <>
+            <Header/>
+            <div style={{marginTop:"80px"}}>
             <List/>
-            <Footer />
+            </div>
         </>
-
     )
 }
 
