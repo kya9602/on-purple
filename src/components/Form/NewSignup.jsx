91ae0c189c67365cef365b24fe22a6a783d550fe
jeongseoe <gldi1633@naver.com>
2022-10-10 17:26:12 +0900

    style : 안쓰는 import 제거함

--- src/components/Form/NewSignup.jsx
+++ src/components/Form/NewSignup.jsx
@@ -11,15 +11,12 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../../assets/images/perple.jpg";
 import { makeStyles } from '@mui/styles';
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router";
-import React, { useState, useRef, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import profileImage from "../../assets/images/profile.jpg";
 import { __checkUsername, __checkNickname } from "../../redux/modules/user";
-import { __getUser } from "../../redux/modules/signup";
 import { __logout, logout } from "../../redux/modules/user";
-import { getSelectUnstyledUtilityClass } from "@mui/base";
 
 
 
