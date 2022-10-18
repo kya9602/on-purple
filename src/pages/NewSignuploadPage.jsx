952775e07f510286049c2355752dd680ddf55d17
Mintaek2 <789rkd@naver.com>
2022-10-18 20:49:47 +0900

    feat :  신고기능 추가

--- src/pages/NewSignuploadPage.jsx
+++ src/pages/NewSignuploadPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import NewSignup from "../components/Form/NewSignup";
-
+import styled from "styled-components";
 const NewSignUpPage = () => {
     return (
-        <NewSignup />
+        <Container>
+            <NewSignup />
+        </Container>
     );
 }
 
 export default NewSignUpPage;
+
+const Container = styled.div`
+    max-width: 428px;
+    width : 100%;
+    margin:0 auto;
+`
--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ChatHeader from "../components/Chating/ChatHeader";
 import Report from "../components/Information/Report";
-
+import styled from "styled-components";
 const ReportPage = () => {
     return (
-        <div>
-        <ChatHeader />
-        <Report />
-        </div>
+        <Container>
+            <ChatHeader />
+            <Report />
+        </Container>
     )
 }
 
 export default ReportPage;
+
+const Container = styled.div`
+    max-width: 428px;
+    width : 100%;
+    margin:0 auto;
+`
