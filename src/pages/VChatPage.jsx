f7caf6329f66a7f0bf24ea7d432e9455fc6d12ec
kya9602 <pok_lewin@naver.com>
2022-10-18 17:52:34 +0900

    화상채팅 서버 구현, 연동 완료

---
+++ src/pages/VChatPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoChatConnect from "../components/VideoChat/VideoChatConnect";
+
+const VChatPage = () => {
+    return (
+        <>
+            <VideoChatConnect />
+        </>
+
+    )
+}
+
+export default VChatPage;
