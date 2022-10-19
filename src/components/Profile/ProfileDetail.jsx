24317798e1c81d4f6ec642d023a62a84cfe30fe6
jeongseoe <gldi1633@naver.com>
2022-10-19 14:14:39 +0900

    style : 뷰만잡아둠

--- src/components/Profile/ProfileDetail.jsx
+++ src/components/Profile/ProfileDetail.jsx
@@ -16,23 +16,23 @@ const ProfileDetail = () => {
             <div>
                 <ImageCard></ImageCard>
             </div>
-        
-        <IntroduceCard>
-            <div>
-                <h3>Nickname 나이</h3>
-            </div>
-            
-            <div>
-                안녕하세요~
-                친구 같은 연애를 하고 싶습니다!
-                술을 즐기는 사이가 되고싶어요~
-            </div>
 
-            <ButtonContainer>
-                <MatchingButton> 대화하기 </MatchingButton>
-            </ButtonContainer>
-            
-        </IntroduceCard>
+            <IntroduceCard>
+                <div>
+                    <h3>Nickname 나이</h3>
+                </div>
+
+                <div>
+                    안녕하세요~
+                    친구 같은 연애를 하고 싶습니다!
+                    술을 즐기는 사이가 되고싶어요~
+                </div>
+
+                <ButtonContainer>
+                    <MatchingButton> 대화하기 💬 </MatchingButton>
+                </ButtonContainer>
+
+            </IntroduceCard>
         </Container>
     )
 }
@@ -40,37 +40,56 @@ const ProfileDetail = () => {
 export default ProfileDetail;
 
 const Container = styled.div`
+    margin-top: 150px;
     display: flex;
     justify-content: center;
+    flex-direction: column;
     align-items:center;
-    min-height: 100vh;
+    height: auto;
+    width: 430px;
 `
 
 const ImageCard = styled.div`
     background-image: url(http://www.joseilbo.com/gisa_img_origin/16546678821654667882_joseedu_origin.jpg);
-    width: 350px;
-    height: 50vh;
+    width: 200px;
+    height: 200px;
     border-radius:20px;
     background-size: cover;
     background-position: center;
     box-shadow: 0px 18px 53px 0px rgba(0, 0, 0, 0.3);
 `
-const MatchingButton = styled.button`
-`
+
 
 const ButtonContainer = styled.div` 
     display: flex;
     justify-content: center;
     align-items:center;
-    margin-top: 270px;
+    margin-top: 100px;
 `
 
+const MatchingButton = styled.button`
+    border: 1px solid skyblue;
+    padding: 4%;
+    cursor: pointer;
+    font-weight: 600;
+    font-size:16px;
+    background-color: white;
 
+    :hover{
+        border: 1px solid skyblue;
+        background-color: skyblue;
+        font-weight: 600;
+        font-size:16px;
+        padding: 4%;
+
+    }
+`
 
 const IntroduceCard = styled.div`
-    margin-left: 20px;
+    margin-top: 20px;
     width: 350px;
-    height: 50vh;
+    height: 150px;
+    word-break: keep-all;
     border-radius:20px;
     border: 1px solid gray;
     box-shadow: 0px 18px 53px 0px rgba(0, 0, 0, 0.3)
