919c4d400378edadbd6716b9541637e7fc69f3f0
jeongseoe <gldi1633@naver.com>
2022-10-20 17:34:39 +0900

    feat : 상대 프로필페이지 불러오기 완료

--- src/redux/modules/chatRoom.js
+++ src/redux/modules/chatRoom.js
@@ -4,43 +4,44 @@ import axios from "axios";
 
 //채팅방 목록 조회
 export const __getChatrooms = createAsyncThunk(
-    "GET_CHATROOMS",
-    async (_, thunkAPI) => {
-      try {
-        const data = await axios.get(`${process.env.REACT_APP_HOST}/chat/room`);
-        console.log(data)
-        return thunkAPI.fulfillWithValue(data);
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
+  "GET_CHATROOMS",
+  async (_, thunkAPI) => {
+    try {
+      const data = await axios.get(`${process.env.REACT_APP_HOST}/chat/room`);
+      console.log(data)
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-  );
+  }
+);
 
 
-  
+
 const initialState = {
-    roomlist:[],
-    };
-  
-  export const chatroom = createSlice({
-      name: "chatroom",
-      initialState,
-      reducers: {},
-  
-      extraReducers: {
-          [__getChatrooms.pending]: (state) => {
-            state.isLoading = true; 
-          },
-          [__getChatrooms.fulfilled]: (state, action) => {
-            state.isLoading = false; 
-            state.roomlist = action.payload;    
-          },
-          [__getChatrooms.rejected]: (state, action) => {
-            state.isLoading = false; 
-            state.error = action.payload; 
-          },
-      }
-    });
-  
-  
-  export default chatroom.reducer;
+  roomlist: [],
+};
+
+export const chatroom = createSlice({
+  name: "chatroom",
+  initialState,
+  reducers: {},
+
+  extraReducers: {
+    [__getChatrooms.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__getChatrooms.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.roomlist = action.payload;
+      console.log(action)
+    },
+    [__getChatrooms.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  }
+});
+
+
+export default chatroom.reducer;
