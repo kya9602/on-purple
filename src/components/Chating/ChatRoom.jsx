64a9b127ebfee9b134c6916fbf92d8af1eaffb23
Mintaek2 <789rkd@naver.com>
2022-10-24 23:09:08 +0900

    test : test code 추가

--- src/components/Chating/ChatRoom.jsx
+++ src/components/Chating/ChatRoom.jsx
@@ -7,8 +7,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { __getlastMessage } from '../../redux/modules/chatRoom';
 
 /* import * as StompJs from "@stomp/stompjs";
-import * as SockJS from "sockjs-client";
- */
+import * as SockJS from "sockjs-client"; */
+
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 
@@ -24,6 +24,7 @@ function ChatRoom() {
     console.log(messages)
     useEffect(() => {
         let sock = new SockJS(process.env.REACT_APP_CHAT_SOCK);
+        //1. SockJS를 내부에 들고있는 stomp를 내어줌
         let client = Stomp.over(sock);
         ws.current = client;
         dispatch(__getlastMessage(roomId))
@@ -39,7 +40,8 @@ function ChatRoom() {
     const ws = useRef();
     
     const token = localStorage.getItem("Authorization")
-    const writer = localStorage.getItem("nickname")
+
+    //2. connection이 맺어지면 실행
     function wsConnect() {
         try {
             console.log(`소켓 연결을 시도합니다.`);
@@ -49,15 +51,16 @@ function ChatRoom() {
             ws.current.connect({ Authorizaion : token , type:"TALK" }, () => {
                 // connect 이후 subscribe
                 console.log('연결 성공')
+                //4. subscribe(path, callback)으로 메세지를 받을 수 있음
                 ws.current.subscribe(`/sub/chat/message`, (response) => {
-                    console.log(response)
                     const newMessage = JSON.parse(response.body);
+                    console.log(newMessage)
                 });
                 // 입장 시 enter 메시지 발신
-                // 이 메시지를 기준으로 서버에서 unReadCount 판별
                 const message = {
                     roomId: roomId,
                 };
+                //3. send(path, header, message)로 메세지를 보낼 수 있음
                 ws.current.send(`/pub/chat/enter`, { Authorizaion : token } , JSON.stringify(message));
             });
         } catch (error) {
@@ -70,7 +73,7 @@ function ChatRoom() {
     function wsDisConnect() {
         try {
             ws.current.disconnect(() => {
-                ws.current.unsubscribe(roomId);
+                ws.current.unsubscribe("sub-0");
             });
         } catch (error) {
         }
@@ -99,18 +102,114 @@ function ChatRoom() {
         setText(e.target.value);
     }, []);
 
-
+ /*    useEffect(() => {
+        connect();
+    
+        return () => disconnect();
+      }, []);
+      const token = localStorage.getItem("Authorization")
+      const client = useRef({});
+      const connect = () => {
+        client.current = new StompJs.Client({
+          //websocket 주소만 입력 가능 * ws://, wss:// 로 시작
+          // brokerURL: "ws://54.180.142.30/ws-stomp/websocket",
+          brokerURL: "wss://sparta521.shop/stomp/chat/websocket",
+          connectHeaders: {
+            Authorization: token,
+          },
+          debug: function (str) {
+            console.log(str);
+          },
+          reconnectDelay: 5000,
+          heartbeatIncoming: 4000,
+          heartbeatOutgoing: 4000,
+          onConnect: () => {
+            //구독요청
+            subscribe();
+            //입장 메시징(type0)
+            client.current.publish({
+              destination: "/pub/chat/enter",
+              headers: { Authorization: token },
+              //전송할 데이터를 입력
+              body: JSON.stringify({
+                type:"TALK",
+                message: "1",
+                roomId: roomId,
+              }),
+            });
+            var heartbeat_msg = '--heartbeat--', heartbeat_interval = null;
+            // missed_heartbeats = 0;
+            if (heartbeat_interval === null) {
+              // missed_heartbeats = 0;
+              heartbeat_interval = setInterval(function() {
+                  try {
+                      // missed_heartbeats++;
+                      // if (missed_heartbeats >= 6)
+                          // throw new Error("Too many missed heartbeats.");
+                      client.current.publish({                                     
+                        destination: "/pub/chat/enter", 
+                        headers: { Authorization: token },
+                        //전송할 데이터를 입력
+                        body: JSON.stringify({
+                          type:"TALK",                        
+                          message: heartbeat_msg,
+                          roomId: roomId.id,
+                          // userId:userId,
+                        }),
+                      });
+                  } catch(e) {
+                      clearInterval(heartbeat_interval);
+                      // const heartbeat_interval = null;
+                      // console.warn("Closing connection. Reason: " + e.message);
+                      // console.log("짱많이 보냄")
+                      // disconnect();
+                  }
+              }, 3000);
+          }
+          },
+        });
+        client.current.activate();
+      };
+      const subscribe = () => {
+        client.current.subscribe(`/sub/chat/message`, function (chat) {
+          const content = JSON.parse(chat.body);
+           console.log(chat)
+        });
+      };
+      const disconnect = () => {
+        //퇴장메시징(type1)
+        client.current.publish({
+          destination: "/pub/chat/enter",
+          headers: { Authorization: token },
+          //전송할 데이터를 입력
+          body: JSON.stringify({
+            type: "TALK",
+            message: "1",
+            roomId: roomId,
+          }),
+        });
+        //구독해제
+        client.current.unsubscribe();
+        //웹소켓 비활성화
+        client.current.deactivate();
+    
+        navigate("/chat");
+      }; */
     return (
         <BackImage>
             <Container>
                 {/* <ChatHeader /> */}
+                {/* <Screenbox>
+                <MyMessage>안녕</MyMessage>
+                <YouMessage>안녕</YouMessage>
+                </Screenbox> */}
                 <ChatContainer>
-                    <Input value={text}
-                        onChange={onChangeChatHandler}
+                    <Input /* value={text}
+                        onChange={onChangeChatHandler} */
                         placeholder="메세지를 입력하세요..."
                         type="text"
                     />
-                    <InputButton onClick={onSend}>전송</InputButton>
+                    <InputButton /* onClick={onSend} */>전송</InputButton>
                 </ChatContainer>
 
             </Container>
