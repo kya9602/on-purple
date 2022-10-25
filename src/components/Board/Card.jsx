e76dbf47525dd01b68a06367675529ad753f35b9
Mintaek2 <789rkd@naver.com>
2022-10-26 04:47:56 +0900

    style : card 크기 조정, 타이틀 길이 제한

--- src/components/Board/Card.jsx
+++ src/components/Board/Card.jsx
@@ -77,10 +77,10 @@ const Item = styled.div`
     max-width: 428px;
     border: 2px solid #9C7FCB;
     border-radius: 5px;
-    width: 100%;
+    width: 90%;
     height: 470px;
     box-shadow:5px 5px 5px grey;
-    margin: 15px 15px 15px 15px;
+    margin: 15px 15px 15px 22px;
     box-sizing: inherit;
 `
 
@@ -97,11 +97,20 @@ const Title = styled.div`
     font-size: 20px;
     float: left;
     margin: 10px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 300px;
+    
 `
 
 const Content = styled.div`
     float: left;
     margin-left: 10px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 300px;
 `
 
 const Container = styled.div`
