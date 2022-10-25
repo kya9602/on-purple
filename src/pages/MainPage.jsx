2d6fed79e57458fe7fd33c566543aa12bffdfc09
Mintaek2 <789rkd@naver.com>
2022-10-25 23:43:06 +0900

    design : main page 좋아요 싫어요 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,7 +4,8 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Deck from "../components/Main/Deck";
 import image from "../assets/images/배경화면으로.jpg"
-
+import BrokenHeart from "../assets/icons/broken-heart.png"
+import Heart from "../assets/icons/heart.png"
 //import Test from "../components/Main/test";
 
 const MainPage = () => {
@@ -14,6 +15,16 @@ const MainPage = () => {
             <MainPageContainer>
                 <Header />
                 <Deck />
+                <HeartBox>
+                    <Left>
+                        <img src={BrokenHeart} alt=""/>
+                        <span>UnLike</span>
+                    </Left>
+                    <Right>
+                        <img src={Heart} alt=""/>
+                        <span>Like</span>
+                    </Right>
+                </HeartBox>
                 <Footer />
             </MainPageContainer>
         </BackImage>
@@ -45,4 +56,43 @@ const BackImage = styled.div`
     background-size: cover;
     height: 100vh;
 
+`
+
+const HeartBox = styled.div`
+  width: 100%;
+  height: 60px;
+  margin-top: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  span{
+    font-size: 15px;
+  }
+`
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 20px;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `
