000d2818ed27436813d9cb55ce7a615df1a703c8
Mintaek2 <789rkd@naver.com>
2022-10-25 18:28:27 +0900

    design : 커뮤니티 가이드 라인 추가

--- src/components/Board/List.jsx
+++ src/components/Board/List.jsx
@@ -10,13 +10,14 @@ import Pagination from "./Pagination/Pagination";
 import searchIcon from "../../assets/icons/search.png"
 import Modal from "@mui/material/Modal"
 import Box from '@mui/material/Box';
+import { Link } from "react-router-dom";
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 428,
     bgcolor: 'background.paper',
     border: '1px solid #ABA1B0',
     boxShadow: 24,
@@ -94,43 +95,64 @@ const List = () => {
     }
     // 게시판 이용 주의 사항 모달 만들 예정.
     return (
-        <>
-            <Wrapper>
-                <CategoryContaier>
-                    <CategoryBox onClick={goDrive}>드라이브 가실 분?</CategoryBox>
-                    <CategoryBox onClick={goTaste}>맛집 추천</CategoryBox>
-                    <CategoryBox onClick={goDate}>데이트 코스 추천</CategoryBox>
-                    <CategoryBox onClick={goMeet}>번개 만남</CategoryBox>
-                    <CategoryBox onClick={goBar}>술 한잔 하실 분?</CategoryBox>
-                    <CategoryBox onClick={goFashion}>패션 질문</CategoryBox>
-                </CategoryContaier>
-                <SearchCautionWrap>
-                    <ModalSpan>
-                        <span onClick={handleOpen}>게시판 이용 주의사항 📄</span>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="modal-modal-title"
-                            aria-describedby="modal-modal-description"
-                        >
-                            <Box sx={style}>
-                                <BoxContainer>
-                                    <h2>새로운 사람을 만나러 가 볼까요?</h2>
-                                    <div>
+    <>
+        <Wrapper>
+            <CategoryContaier>
+                <CategoryBox onClick={goDrive}>드라이브 가실 분?</CategoryBox>
+                <CategoryBox onClick={goTaste}>맛집 추천</CategoryBox>
+                <CategoryBox onClick={goDate}>데이트 코스 추천</CategoryBox>
+                <CategoryBox onClick={goMeet}>번개 만남</CategoryBox>
+                <CategoryBox onClick={goBar}>술 한잔 하실 분?</CategoryBox>
+                <CategoryBox onClick={goFashion}>패션 질문</CategoryBox>
+            </CategoryContaier>
+            <SearchCautionWrap>
+                <ModalSpan>
+                    <span onClick={handleOpen}>커뮤니티 가이드 라인 📄</span>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <BoxContainer>
+                                <h2>커뮤니티 가이드 라인 📄</h2>
+                                <h3>가이드 라인을 위반하는 행동을 발견한다면 <br/>언제든지 <Link to={"/report"}>신고🚨</Link>해주세요.</h3>
+                                <ReportList>
+                                    <li>❗️ 나체 / 성적인 콘텐츠</li>
+                                    <Content> 나체 사진, 성적으로 노골적인 콘텐츠는 금지됩니다.</Content>
+                                    <li>❗️ 사칭</li>
+                                    <Content>자신을 있는 그대로 보여주세요! 타인인 척 행동하지 마세요</Content>
+                                    <li>❗️ 사기, 광고, 불법</li>
+                                    <Content>
+                                        On Purple 을 불법적인 행위를 위한 수단으로 사용하지 마세요.<br/>
+                                        사기 행위, 외부 앱으로 유도하는 광고 또한 금지 되어있습니다.
+                                    </Content>
+                                    <li>❗️ 미성년자</li>
+                                    <Content>
+                                        On Purple 을 이용하려면 만 18세 이상이어야 합니다. <br/>
+                                        미성년자의 독사진을 포함하고 있거나 미성년자 가해를 부추기거나,
+                                        미성년자를 성적이거나 성적인 암시가 포함된 방식으로 묘사하는 프로필을 발견한다면 신고해주세요.
+                                    </Content>
+                                    <li>❗️ 폭력, 위협</li>
+                                    <Content>
+                                        폭력적인 콘텐츠, 노골적인 콘텐츠 또는 유혈 콘텐츠가 허용되지 않습니다.  
+                                        신체적 폭행, 강압, 그리고 모든 종류의 폭력 행위는 엄격하게 금지됩니다.
+                                        자살이나 자해를 미화하거나 옹호하는 콘텐츠도 허용되지 않습니다
+                                    </Content>
+                                </ReportList>
+                            </BoxContainer>
+                        </Box>
+                    </Modal>
+                </ModalSpan>
+                <GoSearch onClick={goSearch}><img src={searchIcon} alt="" /></GoSearch>
+            </SearchCautionWrap>
 
-                                    </div>
-                                </BoxContainer>
-                            </Box>
-                        </Modal>
-                    </ModalSpan>
-                    <GoSearch onClick={goSearch}><img src={searchIcon} alt="" /></GoSearch>
-                </SearchCautionWrap>
-
-                {currentPosts?.map((item) => (<Card item={item} key={item?.postId} />))}
-                <TopButton />
-            </Wrapper>
-            <Pagination postsPerPage={postsPerPage} totalPosts={post?.length} paginate={paginate} />
-        </>
+            {currentPosts?.map((item) => (<Card item={item} key={item?.postId} />))}
+            <TopButton />
+        </Wrapper>
+        <Pagination postsPerPage={postsPerPage} totalPosts={post?.length} paginate={paginate} />
+    </>
     )
 }
 
@@ -206,8 +228,13 @@ const BoxContainer = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
+    ul{
+        float: left;
+    }
 `
 const ModalSpan = styled.div`
+    text-align: center;
+    width: 100%;
     span{
         font-size: 15px;
         font-weight: 600;
@@ -215,4 +242,22 @@ const ModalSpan = styled.div`
         padding-left: 55px;
         cursor: pointer;
     }  
+`
+
+const ReportList = styled.div`
+    li{
+        padding: 5px 0px 5px 5px;
+        list-style: none;
+        margin-bottom: 5px;
+        border-bottom: 1px solid #efefef;
+        font-size: 15px;
+        font-weight: 600;
+        
+        margin-top: 10px;
+    }
+`
+const Content = styled.div`
+    white-space: pre-wrap;
+    font-size: 15px;
+    margin-top: 15px;
 `
