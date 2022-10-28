ab5d329b531633ba022dee2c9a8fbc722ed9c2fe
jeongseoe <gldi1633@naver.com>
2022-10-28 16:21:00 +0900

    글씨짤리는거 수정함

--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -180,21 +180,11 @@ const StLoginContainer = styled.div`
   margin-top: 120px;
   background-color: white;
   max-width: 428px;
-  width: 98.5%;
-  height: auto;
+  width: 370px;
   padding-top: 10px;
   padding-bottom: 5%;
   border: 3px solid #adaaad;
   /* background-color: red; */
-  @media all and (max-width: 880px) {
-     width: 600px;
-  }
-  @media all and (max-width : 680px) {
-    width: 400px;
-  }
-  @media all and (max-width : 480px) {
-    width: 350px;
-  }
 `;
 
 
@@ -211,13 +201,8 @@ const StPwBox = styled.div`
   display: flex;
   justify-content:center;
   margin-top: 20px;
-  margin-bottom: 30px;
-  @media all and (max-width: 750px) {
-    margin-bottom: 15px;
-  }
-  @media all and (max-width : 400px) {
-    margin-bottom: 10px;
-  }
+  margin-bottom: 20px;
+
 `;
 
 //인풋창 디자인
@@ -245,12 +230,7 @@ const StBtnBox = styled.div`
   justify-content: center;
   display: flex;
   margin-top: 20px;
-  @media all and (max-width : 750px) {
-    margin-top: 10px;
-  }
-  @media all and (max-width : 400px) {
-    margin-top: 10px;
-  }
+ 
 `;
 
 //로그인버튼 디자인
@@ -260,40 +240,14 @@ const StBtn = styled.button`
   border: none;
   padding: 1%;
   font-weight: bold;
-  font-size: 17px;
+  font-size: 14px;
   border-bottom-style:solid; 
   border-bottom-color:#80036f;
   border-bottom-width:2px;
   cursor: pointer;
-@media all and (max-width: 750px) {
-    font-size : 14px;
-  }
+
 `
 
-// const StBtnKaka = styled.button`
-//   margin-top: 10px;
-//   background-color: white;
-//   border:none;
-//   cursor: pointer;
-//   @media all and (max-width: 750px) {
-//     img{
-//   width: 140px;
-//       }
-//   }
-
-// `;
-
-// const StBtnNaver = styled.button`
-//   background-color: white;
-//   border:none;
-//   margin-bottom: 10px;
-//   cursor: pointer;
-//   @media all and (max-width: 750px) {
-//     img{
-//   width: 140px;
-//       }
-//   }
-// `;
 
 //회원가입 버튼 박스
 const StJoinBtnBox = styled.div`
